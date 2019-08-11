429e9efd82bcfe5b93c88c71467cc6db7fafbd5d
soyelyuki <n.miranda.meza@gmail.com>
2019-08-11 14:37:18 -0400

    variables

---
+++ 2-variables.js
@@ -0,0 +1,8 @@
+'use strict'
+// Variables
+// una variable es un contednedor de informacion 
+
+ var pais ="españa";
+ var continente ="europa";
+ var antiguedad = 2019
+console.log(pais,continente,antiguedad);
