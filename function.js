456c388328c6e90db84ca3a5b6d6c7a970fc44c4
soyelyuki <n.miranda.meza@gmail.com>
2019-08-11 14:29:42 -0400

    functions

---
+++ function.js
@@ -0,0 +1,15 @@
+
+//performing a task 
+//function greet(name) {
+//    console.log('hello '+ name);
+//}
+
+//greet('hjon');
+
+function square(numer) {
+    return number * number;
+
+}
+
+let number = square(2);
+console.log(number)
