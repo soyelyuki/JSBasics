3ed6a3bfeb4fabae7b8ae3fd4fbcc89ae0282da8
soyelyuki <n.miranda.meza@gmail.com>
2019-08-12 20:28:31 -0400

    condicionales

---
+++ 06-condicional.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var edad1 = 31;
+var edad2 = 12; 
+////
+if (edad1 > edad2){
+    console.log("chupalo1");
+}else{
+    console.log("chupalo2");
+}
+////   !=   diferente 
+
+var edad = 8;
+var nombre ="nicolas";
+
+if(edad >= 18){
+console.log(nombre+"tiene" + edad + "años");
+    if(edad == 33) {
+        console.log("todavia eres");
+    }else if(edad >=70){
+        console.log("ya no eres millenial");
+    }
+}else{
+    console.log(nombre + "tiene "+ edad + "menos");
+}
+// operadores logicos 
+//  AND = Y = &&
+//  OR = o  = || 
+//  negacion = !-
+
