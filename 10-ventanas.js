6e6b860314a78201e04370b52d086fc9cd212d62
soyelyuki <n.miranda.meza@gmail.com>
2019-08-12 23:49:33 -0400

    ventanas de dialogo

---
+++ 10-ventanas.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// alerta 
+
+//alert("esta es mi alerta ");
+
+
+//var mi_resultado = confirm("estas  seguro? ");
+//console.log(mi_resultado);
+
+//ingreso datos
+
+var mi_resultado = prompt ("que edad tienes ? ", 18);  // pregunta, parametro por defecto  
+if( mi_resultado >= 18 ){
+      console.log("chupalo");
+}else{
+        console.log (" ya me tire ");
+}
