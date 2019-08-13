6e383225b92173d8258641a66e5a7d58fe64d907
soyelyuki <n.miranda.meza@gmail.com>
2019-08-12 20:28:06 -0400

    tipos de dato

---
+++ 05-tiposdedatos.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// operadores 
+var  numero1 = 7; 
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+alert(" el resultado es " + operacion);
+
+
+
+//tipos de datos 
+
+var numero_entero = 44; 
+var cadena_texto =' hola "que" tal ';
+var verdadero_o_false = true;   //false
+
+var numer_falso = "33";
+
+console.log(Number(numer_falso )+ 7 );
+
+//con parseint lo puedo convertir a entero y con parsefloat lo puedo convertir en decimal
+// con string lo conviertes en string   
+
+
+// console.log(typeof(cadena_texto)); podemos saber el  tipo de datos que es la variable
