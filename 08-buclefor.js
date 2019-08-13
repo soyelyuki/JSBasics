c7c225e2e0bc250f8cb120963fb6b8f01f1903e9
soyelyuki <n.miranda.meza@gmail.com>
2019-08-12 23:37:08 -0400

    bucle for

---
+++ 08-buclefor.js
@@ -0,0 +1,17 @@
+'use strict'    
+// Bucle for 
+// estructura de control que se repite varias veces
+//   
+
+var numero =100;
+
+for(var i = 0; i <= numero; i++) // primero (x,x,x) estructura del bucle ,  la primera x es la variable 
+                                    // la segunda es la  condicion     
+                                    // y la tercera es como aumenta la condicion   
+                                    {                                
+ 
+    console.log("vamos por el numero " +i); 
+}
+
+
+
