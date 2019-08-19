16d15c40d66be71db263d9cebe9ff69b59e1a475
soyelyuki <n.miranda.meza@gmail.com>
2019-08-18 20:28:08 -0400

    parametros

---
+++ 20-parametros2.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function listadoFrutas(fruta1,fruta2,...resto_de_frutas){
+    console.log("fruta 1; ",fruta1);
+    console.log("fruta 2; ",fruta2 );
+    console.log(resto_de_frutas);
+}
+
+listadoFrutas("naranja","manzana","sandia","pera","melon","coco");
+
+var frutas =[,"naranja","manzana"];
+listadoFrutas("naranja","manzana","sandia","pera","melon","coco");
