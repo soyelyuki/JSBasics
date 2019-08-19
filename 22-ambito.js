a2d00f6a912495845990b958fc58e84cbe07ca99
soyelyuki <n.miranda.meza@gmail.com>
2019-08-18 20:31:36 -0400

    Ambitos

---
+++ 22-ambito.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var texto= "hola mundo soy una variable global";
+function holaMundo(texto){
+    var hola_mundo =" texto dentro de funcion"
+    console.log (hola_mundo)
+    console.log(texto);
+    console.log(numero.toString);
+}
+
+var numero = 12;
+holaMundo(texto);
+
+//console.log(hola_mundo);
